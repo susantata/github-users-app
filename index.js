ab1cec2ea92d38a73a3db279129f0cde99f2d8ba
+ <+>
2022-09-01 13:00:35 +0300

    added some part of js

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-let title = card.querySelector (".title");
-let img = card.querySelector (".img");
-let commentForm = card.querySelector ("commentForm");
+const input = document.querySelector ("input");
+const btn = document.querySelector ("viwerbtn");
+const viwer = document.querySelector ("githubviwer");
+const logi = document.querySelector ("githubusername");
+const joined = document.querySelector ("githubjoineddata");
+const repo = document.querySelector ("repoTotal");
+const follower = document.querySelector ("followerTotal");
+const following = document.querySelector ("followingTotal");
+const location = document.querySelector ("location");
+const twit = document.querySelector ("twit");
+const website = document.querySelector ("website");
+const companies = document.querySelector ("companies");
+const gitbio= document.querySelector ("githubbio");
