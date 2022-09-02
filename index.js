1abcf0fa44a9580f919c495afc39e13324b8d954
+ <+>
2022-09-02 10:27:31 +0300

    working in js

--- index.js
+++ index.js
@@ -7,4 +7,24 @@ const  followers = document.querySelector(".followers");
 const repos = document.querySelector(".repos");
 const  btn = document.querySelector("#btn");
 const url = `https://api.github.com/user/${userName}`;
-document.addEventListener("DOMContentLoaded",fetchInfo)
+
+
+document.addEventListener("DOMContentLoaded",fetchInfo) => {
+    searchbutton.addEventListener('click',() => {
+       
+    })
+}
+
+function fetchInfo{
+fetch("https://api.github.com/users/user_name/repos")
+.then(res => res.json())
+.then(Input => {
+    Input.value = userName;
+    img.src = `${img.profile}`;
+    searchbutton.querySelector('click',() => {
+Input.following = Input.following +1;
+Input.followers = Input.folowers +1;
+Input.repos = Input.repos +1;
+    })
+})
+}
