f5711f2938163b4e5bfaae54e1bdbca811ee0c3a
+ <+>
2022-09-02 12:58:31 +0300

    made changes on css

--- index.js
+++ index.js
@@ -1,30 +1,64 @@
 //https://api.github.com/users/user_name/repos
 
 const user_img = document.querySelector(".profile");
-const userName = document.querySelector("#input").value;
+const input = document.querySelector("#input");
 const following = document.querySelector(".following");
 const  followers = document.querySelector(".followers");
-const repos = document.querySelector(".repos");
-const  btn = document.querySelector("#btn");
-const url = `https://api.github.com/user/${userName}`;
+const repo = document.querySelector(".repos");
+const  btn = document.querySelector(".btn");
+const url = "https://api.github.com/users/";
+const username = document.querySelector('.username');
+const bio = document.querySelector('.bio')
+const plink = document.querySelector('.plink');
+
+ 
+document.addEventListener("DOMContentLoaded", () =>{
+    btn.addEventListener('click', () => {
+        fetchInfo();
+        fetchRepos();
+        fetchFollowers();
+        fetchFollowing();
+    })
+});
+
+function fetchInfo() {
+   
+    return fetch(url + input.value)
+            .then(resp => resp.json())
+        .then(data => {
+                console.log(data)
+                user_img.src = data.avatar_url;
+                username.textContent = `Username:  ${data.login}`;
+                bio.textContent = `Bio:  ${data.bio}`
+                plink.textContent = `Github:  ${data.html_url}`
+
+        })
+}
 
 
-document.addEventListener("DOMContentLoaded",fetchInfo) => {
-    searchbutton.addEventListener('click',() => {
-       
+function fetchRepos() {
+    return fetch(url + input.value + `/repos?per_page=100`)
+        .then(resp => resp.json())
+        .then(repos => {
+            console.log(repos)
+            repo.textContent = repos.length;
     })
 }
 
-function fetchInfo{
-fetch("https://api.github.com/users/user_name/repos")
-.then(res => res.json())
-.then(Input => {
-    Input.value = userName;
-    img.src = `${img.profile}`;
-    searchbutton.querySelector('click',() => {
-Input.following = Input.following +1;
-Input.followers = Input.folowers +1;
-Input.repos = Input.repos +1;
+function fetchFollowing() {
+    return fetch(url + input.value + `/following`)
+        .then(resp => resp.json())
+        .then(flw => {
+            console.log(flw)
+            following.textContent = flw.length;
     })
-})
+}
+
+function fetchFollowers() {
+    return fetch(url + input.value + `/followers`)
+        .then(resp => resp.json())
+        .then(folo => {
+            console.log(folo)
+            followers.textContent = folo.length;
+    })
 }
